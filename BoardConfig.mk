00fcc2413cc650a862322816ff16a5368b3e6652
Rolex040623 <saurabhpattebahadur7@gmail.com>
2026-05-29 00:03:02 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -162,7 +162,6 @@ TW_NO_FASTBOOT_BOOT := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 TW_POWER_BUTTON_IS_NOT_SCREENSHOT := true
 
-OF_MAINTAINER := Rolex
 OF_SCREEN_H := 2400
 # Device Variants Support
 TARGET_OTA_ASSERT_DEVICE := pissarro,pissarroin,pissarroinpro,pissarropro
