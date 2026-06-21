c100e7d32a434fc7d76a7226e701d58e11d68471
Rolex040623 <saurabhpattebahadur7@gmail.com>
2026-06-21 20:36:16 +0530

    Change TWRP common config inheritance

--- omni_pissarro.mk
+++ omni_pissarro.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, device/xiaomi/pissarro/device.mk)
 
 # Inherit some common TWRP stuff.
-$(call inherit-product, vendor/orangefox/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := pissarro
